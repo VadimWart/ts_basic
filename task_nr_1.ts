4812a618568e0a4e2747d01b11ad54e2b4f2ddb2
VadimWart <wartvadim@gmail.com>
2025-11-11 12:54:45 +0100

    add_task_nr_1

---
+++ task_nr_1.ts
@@ -0,0 +1,20 @@
+const currRate: string = "1.05";
+
+const fetchCurr = (response: string): number => {  	//Rein geht: String ("1.05")
+													//Drin passiert: Umwandlung von String zu Number
+													//Raus kommt: Number (1.05)
+	const data: number  = JSON.parse(response);
+	return data;
+};
+
+function transferEurToUsd(available: boolean, amount: number, commission: number) {
+	if (available) {
+		let res = fetchCurr(currRate) * amount * commission;
+		console.log(res);
+		// Или запись в элемент на странице вместо консоли
+	} else {
+		console.log("Сейчас обмен недоступен");
+	}
+}
+
+transferEurToUsd(true, 500, 1.05);
